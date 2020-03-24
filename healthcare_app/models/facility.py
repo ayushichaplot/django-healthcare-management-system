513ec11f6b2cebc71ac2d8934552e6a1d657a7c0
Ayushi Chaplot <ayushichaplot@Ayushis-MacBook-Air.local>
2020-03-24 12:38:24 +0530

    commit

--- healthcare_app/models/facility.py
+++ healthcare_app/models/facility.py
@@ -10,26 +10,26 @@ from .state import State
 
 class NameField(models.CharField):
     def __init__(self, *args, **kwargs):
-        name_regex = RegexValidator(regex=r'^\d*[a-zA-Z][#.0-9a-zA-Z\s,-]+$',
-                                    message="Name should consist of characters")
-        kwargs['validators'] = [name_regex]
-        kwargs['max_length'] = 100
         super(NameField, self).__init__(*args, **kwargs)
 
     def get_prep_value(self, value):
-        return str(value).lower()
-    
+        return str(value).capitalize()
+
+
 
 
 class Facility(models.Model):
     city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True)
-    name = models.NameField()
+    name_regex = RegexValidator(regex=r'^\d*[a-zA-Z][#.0-9a-zA-Z\s,-]+$',
+                                message="Name should consist of character")
+    name = NameField(validators=[name_regex], max_length=100)
     address_regex = RegexValidator(regex=r'^\d*[a-zA-Z][#.0-9a-zA-Z\s,-]+$',
                                    message="Address should be in proper format")
     address = models.CharField(validators=[address_regex], max_length=100)
     medicare_ccn = models.IntegerField(unique=True)
 
+    class Meta:
+        unique_together = ["city", "name"]
+
     def __str__(self):
         return self.name
-
-
